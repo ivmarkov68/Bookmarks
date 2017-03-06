c154772d8ba564b88bccd96f7bb0b84663210f6f
Ivan Markov <ivmarkov@gmail.com>
2017-03-06 16:32:41 +0200

    Add project files.

---
+++ Bookmarks/BookmarkInfo.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookmarks
+{
+    public class BookmarkInfo
+    {
+        public BookmarkInfo[] children { get; set; }
+
+        public long date_added { get; set; }
+
+        public int id { get; set; }
+
+        public string name { get; set; }
+
+        public int sync_transaction_version { get; set; }
+
+        public string type { get; set; }
+
+        public string url { get; set; }
+    }
+
+    public class BookmarkRoot
+    {
+        public BookmarkInfo[] children { get; set; }
+
+        public long date_added { get; set; }
+
+        public long date_modified { get; set; }
+
+        public int id { get; set; }
+
+        public string name { get; set; }
+
+        public string type { get; set; }
+    }
+
+    public class Roots
+    {
+        public RootsHeader roots { get; set; }
+        public string version { get; set; }
+    }
+
+
+    public class RootsHeader 
+    {
+        public BookmarkRoot bookmark_bar { get; set; }
+        public BookmarkRoot other { get; set; }
+        public string sync_transaction_version { get; set; }
+    }
+}
---
+++ Bookmarks/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.IO;
+
+namespace Bookmarks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filename = @"c:\bookmarks";
+            JObject o1 = JObject.Parse(File.ReadAllText(filename));
+            // read JSON directly from a file
+            using (StreamReader file = File.OpenText(filename))
+            using (JsonTextReader reader = new JsonTextReader(file))
+            {
+                JObject o2 = (JObject)JToken.ReadFrom(reader);
+            }
+
+            var o3 = o1.ToObject<Roots>();
+
+            var z = o3.roots.bookmark_bar.children.Traverse(x => x.children);
+
+        }
+    }
+}
